0945abdd7ca17f296abeb4d8571d1b75654f78bf
Dru-429 <ect.dhruv@gmail.com>
2026-07-28 14:18:07 +0530

    Added Image upload component

--- components/landing.tsx
+++ components/landing.tsx
@@ -5,6 +5,7 @@ import Hero from './landing_ui/Hero';
 import Features from './landing_ui/Features';
 import Footer from './landing_ui/Footer';
 import TemplateEditor from './editor/templateEditor';
+import ImageUpload from './editor/ImageUpload';
 
 export default function LandingPage() {
   return (
@@ -12,6 +13,7 @@ export default function LandingPage() {
       <div className='mx-auto max-w-7xl px-4 py-6 md:px-8 md:py-8'>
         <Navbar />
         <Hero />
+        <ImageUpload />
         <TemplateEditor />
         <Features />  {/* Feature + Gallery + Reviews + Faq */}
         <Footer />
